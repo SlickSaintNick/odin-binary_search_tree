9312ca9f98ea98de8a6e0a9f4bfee8bcc844cd36
SlickSaintNick <144679503+SlickSaintNick@users.noreply.github.com>
2024-01-20 08:09:10 +1030

    Refactored the print methods into a TreePrint class.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -9,6 +9,80 @@ class Node
     @left = left
     @right = right
   end
+
+  def clone
+    Node.new(data, left, right)
+  end
+end
+
+class TreePrinter
+  def initialize(root)
+    @root = root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '|   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '|   '}", true) if node.left
+  end
+
+  def level_order(&block)
+    return if @root.nil?
+
+    queue = [@root]
+    values = []
+
+    until queue.empty?
+      current = queue.shift
+      if block_given?
+        yield current
+      else
+        values.push(current.data)
+      end
+      queue.push(current.left) unless current.left.nil?
+      queue.push(current.right) unless current.right.nil?
+    end
+    return values unless block_given?
+  end
+
+  def inorder(node = @root, values = [], &block)
+    return if node.nil?
+
+    inorder(node.left, values, &block)
+    if block_given?
+      yield node
+    else
+      values.push(node.data)
+    end
+    inorder(node.right, values, &block)
+    values unless values.nil?
+  end
+
+  def preorder(node = @root, values = [], &block)
+    return if node.nil?
+
+    if block_given?
+      yield node
+    else
+      values.push(node.data)
+    end
+    preorder(node.left, values, &block)
+    preorder(node.right, values, &block)
+    values unless values.nil?
+  end
+
+  def postorder(node = @root, values = [], &block)
+    return if node.nil?
+
+    postorder(node.left, values, &block)
+    postorder(node.right, values, &block)
+    if block_given?
+      yield node
+    else
+      values.push(node.data)
+    end
+    values unless values.nil?
+  end
 end
 
 # Tree holds a Balanced BST and associated methods.
@@ -17,6 +91,7 @@ class Tree
 
   def initialize(array)
     @root = build_tree(array.sort.uniq)
+    @printer = TreePrinter.new(@root)
   end
 
   def build_tree(array, start_index = 0, end_index = array.length - 1)
@@ -30,7 +105,9 @@ class Tree
   end
 
   def insert(value)
-    cursor = Node.new(@root.data, @root.left, @root.right)
+    return build_tree([value]) if @root.nil?
+
+    cursor = @root.clone
     cursor = value < cursor.data ? cursor.left : cursor.right until cursor.left.nil? && cursor.right.nil?
     if value < cursor.data
       cursor.left = Node.new(value)
@@ -41,32 +118,32 @@ class Tree
 
   def delete(root = @root, value)
     # Base case
-    return root if root.nil?
+    return nil if root.nil?
 
     # Recursively look through the BST to find the node to be deleted.
     if root.data > value
       root.left = delete(root.left, value)
-      return root
     elsif root.data < value
       root.right = delete(root.right, value)
-      return root
-    end
-
-    # At this point, root is the node to be deleted. No children... delete. One child... swap and delete.
-    # Two children... replace the node with its successor and delete (successor will have max one child).
-    if root.left.nil?
-      return replace_node(root, root.right)
-    elsif root.right.nil?
-      return replace_node(root, root.left)
     else
-      return replace_with_successor(root)
+      # At this point, root is the node to be deleted.
+      # One or no children... swap and delete.
+      # Two children... replace the node with its successor and delete.
+      # (Successor will have max one child by definition).
+      if root.left.nil?
+        root = root.right
+      elsif root.right.nil?
+        root = root.left
+      else
+        return replace_with_successor(root)
+      end
     end
+    root
   end
 
-  def replace_node(origin, replacement)
-    origin = nil
-    replacement
-  end
+  # def replace_node(origin, replacement)
+  #   replacement
+  # end
 
   def replace_with_successor(root)
     successor_parent = root
@@ -87,76 +164,32 @@ class Tree
     root
   end
 
-  def pretty_print(node = @root, prefix = '', is_left = true)
-    pretty_print(node.right, "#{prefix}#{is_left ? '|   ' : '    '}", false) if node.right
-    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '|   '}", true) if node.left
-  end
-
   def find(value)
-    cursor = Node.new(@root.data, @root.left, @root.right)
+    cursor = @root.clone
     until cursor.nil? || cursor.data == value
       cursor = value < cursor.data ? cursor.left : cursor.right
     end
     cursor
   end
 
-  def level_order
-    return if @root.nil?
-
-    queue = [@root]
-    values = []
-
-    until queue.empty?
-      current = queue.shift
-      if block_given?
-        yield current
-      else
-        values.push(current.data)
-      end
-      queue.push(current.left) unless current.left.nil?
-      queue.push(current.right) unless current.right.nil?
-    end
-    return values unless block_given?
+  def pretty_print
+    @printer.pretty_print(@root)
   end
 
-  def inorder(node = @root, values = [])
-    return if node.nil?
-
-    inorder(node.left, values)
-    if block_given?
-      yield node
-    else
-      values.push(node.data)
-    end
-    inorder(node.right, values)
-    values unless values.nil?
+  def level_order(&block)
+    @printer.level_order(&block)
   end
 
-  def preorder(node = @root, values = [])
-    return if node.nil?
-
-    if block_given?
-      yield node
-    else
-      values.push(node.data)
-    end
-    preorder(node.left, values)
-    preorder(node.right, values)
-    values unless values.nil?
+  def inorder(&block)
+    @printer.inorder(&block)
   end
 
-  def postorder(node = @root, values = [])
-    return if node.nil?
+  def preorder(&block)
+    @printer.preorder(&block)
+  end
 
-    postorder(node.left, values)
-    postorder(node.right, values)
-    if block_given?
-      yield node
-    else
-      values.push(node.data)
-    end
-    values unless values.nil?
+  def postorder(&block)
+    @printer.postorder(&block)
   end
 
   def height(root = @root)
@@ -166,7 +199,7 @@ class Tree
   end
 
   def depth(node)
-    cursor = Node.new(@root.data, @root.left, @root.right)
+    cursor = @root.clone
     depth = 0
 
     until node.nil? || cursor.data == node.data
@@ -196,33 +229,52 @@ def press_enter
   gets
 end
 
-# Driver script
-tree = Tree.new(Array.new(15) { rand(1..100) })
+# # Driver script
+# tree = Tree.new(Array.new(15) { rand(1..100) })
+# tree.pretty_print
+# puts "Is the tree balanced? #{tree.balanced?}"
+# press_enter
+# puts 'Elements in order:'
+# puts tree.level_order { |value| "#{value} : "}
+# puts "Level Order: #{tree.level_order}"
+# puts "Preorder:    #{tree.preorder}"
+# puts "Postorder:   #{tree.postorder}"
+# puts "Inorder:     #{tree.inorder}"
+# press_enter
+# puts 'Adding large numbers to unbalance the tree...'
+# tree.insert(120)
+# tree.insert(140)
+# tree.insert(160)
+# tree.insert(180)
+# tree.insert(200)
+# tree.pretty_print
+# puts "Is the tree balanced? #{tree.balanced?}"
+# press_enter
+# tree.rebalance
+# tree.pretty_print
+# puts 'Rebalancing...'
+# puts "Is the tree balanced? #{tree.balanced?}"
+# press_enter
+# puts 'Elements in order:'
+# puts "Level Order: #{tree.level_order}"
+# puts "Preorder:    #{tree.preorder}"
+# puts "Postorder:   #{tree.postorder}"
+# puts "Inorder:     #{tree.inorder}"
+
+# tree = Tree.new(Array.new(15) { rand(1..100) })
+# tree.pretty_print
+# tree.level_order { |node| print "#{node.data}: " if node.data.even? }
+# print "\n"
+# tree.inorder { |node| print "#{node.data}: " if node.data.even? }
+# print "\n"
+# tree.preorder { |node| print "#{node.data}: " if node.data.even? }
+# print "\n"
+# tree.postorder { |node| print "#{node.data}: " if node.data.even? }
+# print "\n"
+
+tree = Tree.new([1, 2, 3, 4, 5])
+tree.delete(5)
+tree.delete(1)
+tree.delete(3)
+
 tree.pretty_print
-puts "Is the tree balanced? #{tree.balanced?}"
-press_enter
-puts 'Elements in order:'
-puts "Level Order: #{tree.level_order}"
-puts "Preorder:    #{tree.preorder}"
-puts "Postorder:   #{tree.postorder}"
-puts "Inorder:     #{tree.inorder}"
-press_enter
-puts 'Adding large numbers to unbalance the tree...'
-tree.insert(120)
-tree.insert(140)
-tree.insert(160)
-tree.insert(180)
-tree.insert(200)
-tree.pretty_print
-puts "Is the tree balanced? #{tree.balanced?}"
-press_enter
-tree.rebalance
-tree.pretty_print
-puts 'Rebalancing...'
-puts "Is the tree balanced? #{tree.balanced?}"
-press_enter
-puts 'Elements in order:'
-puts "Level Order: #{tree.level_order}"
-puts "Preorder:    #{tree.preorder}"
-puts "Postorder:   #{tree.postorder}"
-puts "Inorder:     #{tree.inorder}"
